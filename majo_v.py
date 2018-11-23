2141acc7ae314496e63b54973ea240f866e5b283
RFarkas <r4lv@peaxels.com>
2018-11-23 17:51:15 +0100

    REL: bump version 0.2.1 → 0.2.2

--- majo_v.py
+++ majo_v.py
@@ -7,7 +7,7 @@ import click
 import rumps
 import pendulum
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "r4lv"
 __author_email__ = "r4lv@peaxels.com"
 __url__ = "https://github.com/r4lv/majo-v"
